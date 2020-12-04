381b9f3bd80f27f9c469873370d10407affa0446
Fortyfei <54122927+Fortyfei@users.noreply.github.com>
2020-12-04 23:49:52 +0100

    Hozzáadtam a két json generáló python scriptet

---
+++ termek.py
@@ -0,0 +1,20 @@
+import json
+
+class Product:
+    def __init__(self,name, id,needs):
+        self.name=name
+        self.id=id
+        self.needs=needs
+
+
+print("Új termék létrehozása")
+name=input("Adja meg a termék nevét: ")
+id=input("Adja meg a termék azonosítóját: ")
+print("Adja meg a szükséges gépeket!")
+needs=[]
+s=input()
+while s:
+    needs.append(s)
+    s=input()
+f=open(id+".json","w")
+f.write(json.dumps(Product(name,id,needs).__dict__))
